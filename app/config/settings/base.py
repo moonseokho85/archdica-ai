922a22430f3cb0600b1598c7cff852d883dd3240
Logan Moon <mseokho7733@gmail.com>
2021-04-08 13:21:26 +0900

    update .gitignore and modify settings and entrypoint.sh

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fjeijfksjdfwiovnidsnoifnewiofoidsnksdnoiewnions')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # if True, development mode. else production mode.
